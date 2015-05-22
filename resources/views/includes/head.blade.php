9b7f9d9bc51d51bf145bedaf24f24063c2b158f5
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2015-05-22 18:37:24 +0930

    Mobile menu

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,9 +1,12 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, content=minimal-ui">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, content=minimal-ui">
 <title>Rebecca Cordingley Designs</title>
 
+<link href="{{ asset('/css/reset.css') }}" rel="stylesheet">
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/mediaqueries.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
 
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <?php
 // include(app_path().'/includes/common.php');
-
+//
 // use app\includes;
 //
 // // check if current page matches var and add active class to current menu item
@@ -12,30 +12,68 @@
 ?>
 
 <header class="header">
-    <!-- <h2 class="logo">RJC</h2> -->
-    <img src="{{asset('images/logo3.png')}}" alt="RC logo" class="logo-img">
-    <h1 class="title"><a href="{{ URL::to('/') }}">REBECCA CORDINGLEY DESIGNS</a></h1>
 
-    <ul class="s-m-i">
+    <nav class="mobile visible-xs-block">
+        <div class="row">
+            <div class="col-xs-12">
+                <ul class="mobile-menu">
+                    <li>
+                        <a href="#" id="dropdown-link"><i class="fa fa-bars"></i> <span>Menu</span></a>
+                        <ul class="s-m-i">
+                            <li><a href="http://www.twitter.com/rjcordingley" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.pinterest.com/beccordingley/" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="https://instagram.com/rebeccacdesigns/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                        <ul class="drop" id="dropdown-menu">
+                            <li class="menu-item"><a href="{{URL::to('/')}}">Home</a></li>
+                            <li class="menu-item"><a href="{{ URL::to('about') }}">About</a></li>
+                            <li class="menu-item"><a $class href="http://www.etsy.com" target="_blank">Etsy</a></li>
+                            <li class="menu-item">Shop</li>
+                            <li class="menu-item"><a href="{{ URL::to('gallery') }}">Gallery</a></li>
+                            <li class="menu-item"><a href="{{URL::to('contact') }}">Contact</a></li>
+                            <li class="menu-item">Web Development</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="heading">
+        <div class="row">
+            <div class="col-xs-3 col-sm-12 bottom-align">
+                <img src="{{asset('images/logo3.png')}}" alt="RC logo" class="logo-img">
+            </div>
+            <div class="col-xs-9 col-sm-12 bottom-align">
+                <h1 class="title"><a href="{{ URL::to('/') }}">REBECCA CORDINGLEY DESIGNS</a></h1>
+            </div>
+        </div>
+    </div>
+
+    <ul class="s-m-i hidden-xs">
         <li><a href="http://www.twitter.com/rjcordingley" target="_blank"><i class="fa fa-twitter"></i></a></li>
         <li><a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook"></i></a></li>
         <li><a href="https://www.pinterest.com/beccordingley/" target="_blank"><i class="fa fa-pinterest"></i></a></li>
         <li><a href="https://instagram.com/rebeccacdesigns/" target="_blank"><i class="fa fa-instagram"></i></a></li>
     </ul>
     <div class="clear"></div>
-    <nav class="nav">
 
-        <ul class="menu">
-            <li class="menu-item"><a href="{{URL::to('/')}}">HOME</a></li> |
-            <li class="menu-item"><a href="{{ URL::to('about') }}">ABOUT</a></li> |
-            <li class="menu-item"><a $class href="http://www.etsy.com" target="_blank">ETSY</a></li> |
-            <li class="menu-item">SHOP</li> |
-            <li class="menu-item"><a href="{{ URL::to('gallery') }}">GALLERY</a></li> |
-            <li class="menu-item"><a href="{{URL::to('contact') }}">CONTACT</a></li> |
-            <li class="menu-item">WEB DEVELOPMENT</li>
-        </ul>
+
+    <nav class="nav hidden-xs">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="menu">
+                    <li class="menu-item"><a href="{{URL::to('/')}}">HOME</a></li> |
+                    <li class="menu-item"><a href="{{ URL::to('about') }}">ABOUT</a></li> |
+                    <li class="menu-item"><a $class href="http://www.etsy.com" target="_blank">ETSY</a></li> |
+                    <li class="menu-item">SHOP</li> |
+                    <li class="menu-item"><a href="{{ URL::to('gallery') }}">GALLERY</a></li> |
+                    <li class="menu-item"><a href="{{URL::to('contact') }}">CONTACT</a></li> |
+                    <li class="menu-item">WEB DEVELOPMENT</li>
+                </ul>
+            </div><!-- col-xs-12 -->
+        </div><!-- row -->
     </nav>
-
-    <!-- <h1 class="title"><a href="{{ URL::to('/') }}">REBECCA CORDINGLEY DESIGNS</a></h1> -->
 
 </header>
