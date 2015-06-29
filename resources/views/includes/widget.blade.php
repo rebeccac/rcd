cde006c04e3f306f2d713cd443a147276e6b28ed
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2015-06-29 16:51:03 +0930

    Etsy widget

--- resources/views/includes/widget.blade.php
+++ resources/views/includes/widget.blade.php
@@ -3,7 +3,8 @@
         <div class="widget_price">${{$listings['results'][$rand]['price']}}</div>
         <div class="widget_currency">USD</div>
         <div class="clear"></div>
-        <div class="widget_store_link"><a href="https://www.etsy.com/shop/RebeccaCordingley" target="_blank">View more <br> on Etsy</a></div>
+        <div class="widget_store_link">
+            <a href="https://www.etsy.com/shop/RebeccaCordingley" target="_blank">View more <br> on Etsy</a></div>
     </div>
 
     <div class="widget_right">
@@ -12,6 +13,9 @@
 
         <div class="widget_description">
             <?php echo $description; ?>
+        </div>
+        <div class="etsy_badge">
+            <a href="//www.etsy.com/shop/RebeccaCordingley?ref=offsite_badges&utm_source=sellers&utm_medium=badges&utm_campaign=en_isell_3"><img height="25" src="//img0.etsystatic.com/site-assets/badges/en/en_isell_3.png"></a>
         </div>
     </div>
 </div>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,35 @@
 @section('content')
 <div class="content">
     <h2>About Rebecca Cordingley</h2>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
 
+    <p>
+        I grew up in Adelaide, South Australia, and moved to Melbourne, then the Macedon Ranges in Victoria before deciding to move back to Adelaide in 2014.
+    </p>
+    <p>
+        I'm happiest when I have art in my life. I'm a programmer/web developer (with a couple of other degrees) but I always end up coming back to art.
+    </p>
+    <p>
+        I was a lampwork glass artist for several years, then had a break for a few years while I went back to university to study computer science. I've recently started silversmithing and love it.
+    </p>
+    <p>
+        In between (and still), there's been photography, knitting, crochet, sewing... anything that allows me to use my hands and mind to be creative. Even programming can be surprisingly creative.
+    </p>
+    <p>
+        I love the sense of accomplishment that comes from turning a pile of raw materials into something (hopefully) beautiful or useful.
+    </p>
+    <p>
+        All my items are handmade, by me, in my studio in Adelaide, South Australia. I use recycled/reclaimed materials whenever possible.
+    </p>
+    <p>
+        I hope you like my work.
+    </p>
+
+    <p class="signature">
+        Rebecca Cordingley
+    </p>
+</div>
+</div><!-- row -->
 </div><!-- content -->
 @endsection
